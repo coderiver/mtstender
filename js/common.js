44738799de74aafa26dfec928c7972877976fcdb
moroz <moroz@morozs-Mac-mini.local>
2014-01-17 18:12:24 +0200

    main page

--- js/common.js
+++ js/common.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
 
+//main slider
+function main_slider() {
+	var wnd_height = $(window).height();	
+	var el = $('.js-m-slider');
+	el.height(wnd_height);
+}
+main_slider();
+
+$(window).resize(function(){
+	main_slider();
+});
+
 });
